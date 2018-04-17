35c8df04d82bef3c046cd011f808ccccd7c73369
SamarV-121 <samarvispute121@gmail.com>
2018-04-17 12:10:30 +0530

    nicklaus: Import more blobs
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- nicklaus/Android.mk
+++ nicklaus/Android.mk
@@ -1,2 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter nicklaus, $(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
+endif
