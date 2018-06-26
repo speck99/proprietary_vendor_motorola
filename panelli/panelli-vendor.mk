ef2eccf9f62b40936154775ae4a3d5cde1ba2929
DhirajSurvase <dms25121997@gmail.com>
2018-06-26 05:12:25 +0000

    panelli: fix path for proprietary blobs

--- panelli/panelli-vendor.mk
+++ panelli/panelli-vendor.mk
@@ -1,13 +1,13 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/bin,system/vendor/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/vendor/bin,system/vendor/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/etc,system/vendor/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/vendor/etc,system/vendor/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/firmware,system/vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/vendor/firmware,system/vendor/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/proprietary/vendor/lib,system/vendor/lib)
