e3e65c848010f2e8b6528bd73a9b40dfa8529f0d
DhirajSurvase <dms25121997@gmail.com>
2018-06-26 05:09:28 +0000

    panelli:(Moto C Plus): init
    These blobs are based on alps-7.1 with some stock device specific blobs from stock
    
    imported from https://github.com/DhirajSurvase/android-vendor-Moto-C-plus

---
+++ panelli/README.md
@@ -0,0 +1,7 @@
+## Vendor Tree
+
+device:  Motorola Moto C plus
+
+Thanks to Darklord,Mysteryagr,Alberto Guanti and all other mtk devs and my friends
+
+           
---
+++ panelli/panelli-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary/vendor/lib,system/vendor/lib)
